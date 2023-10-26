866c1c8432127ea4c0cea9f326aa94a39b1f8219
Arend Peter Castelein <apc1993@gmail.com>
2023-10-26 00:00:52 -0700

    Add more stats from rcv cruncher

--- parser.py
+++ parser.py
@@ -18,7 +18,7 @@ def get_args():
 
 def parse_cvrs(file_names):
     data = defaultdict( lambda: {
-        # These are set based on rcv cruncher
+        # These are derived values
         'election': '',
         'competitive_ratio': -1,
         # These are empty unless there's an override
@@ -28,7 +28,36 @@ def parse_cvrs(file_names):
         'downward_monotonicity_failure': '',
         'compromise_failure': '',
         'repeal': '',
-        'sources': []
+        'note1': '',
+        'note2': '',
+        'note3': '',
+        # These are set based on rcv cruncher
+        'n_candidates': '',
+        'rank_limit': '',
+        'restrictive_rank_limit': '',
+        'first_round_overvote': '',
+        'ranked_single': '',
+        'ranked_3_or_more': '',
+        'ranked_multiple': '',
+        'total_ballots': '',
+        'total_fully_ranked': '',
+        'mean_rankings_used': '',
+        'winner': '',
+        'n_rounds': '',
+        'total_pretally_exhausted': '',
+        'total_posttally_exhausted': '',
+        'total_posttally_exhausted_by_overvote': '',
+        'total_posttally_exhausted_by_skipped_rankings': '',
+        'total_posttally_exhausted_by_abstention': '',
+        'total_posttally_exhausted_by_duplicate_rankings': '',
+        'total_posttally_exhausted_by_rank_limit': '',
+        'total_posttally_exhausted_by_rank_limit_fully_ranked': '',
+        'total_posttally_exhausted_by_rank_limit_partially_ranked': '',
+        'first_round_winner_vote': '',
+        'final_round_winner_vote': '',
+        'first_round_winner_percent': '',
+        'final_round_winner_percent': '',
+        'first_round_winner_place': '',
     })
 
     for file in file_names:
@@ -46,6 +75,9 @@ def parse_cvrs(file_names):
              exhaust_on_N_repeated_skipped_marks=0
         )
 
+        stats = election.get_stats()[0].to_dict()
+        stats = {k:v[0] for k,v in stats.items()}
+
         # Compute competitive ratio
         rounds = election.get_round_by_round_dict()
             # TODO: I probably shouldn't assume that the elected will be first
@@ -58,6 +90,33 @@ def parse_cvrs(file_names):
         data[election_name].update({
             'election': election_name,
             'competitive_ratio': competitive_ratio,
+
+            'n_candidates': stats['n_candidates'],
+            'rank_limit': stats['rank_limit'],
+            'restrictive_rank_limit': stats['restrictive_rank_limit'],
+            'first_round_overvote': stats['first_round_overvote'],
+            'ranked_single': stats['ranked_single'],
+            'ranked_3_or_more': stats['ranked_3_or_more'],
+            'ranked_multiple': stats['ranked_multiple'],
+            'total_ballots': stats['total_ballots'],
+            'total_fully_ranked': stats['total_fully_ranked'],
+            'mean_rankings_used': stats['mean_rankings_used'],
+            'winner': stats['winner'],
+            'n_rounds': stats['n_rounds'],
+            'total_pretally_exhausted': stats['total_pretally_exhausted'],
+            'total_posttally_exhausted': stats['total_posttally_exhausted'],
+            'total_posttally_exhausted_by_overvote': stats['total_posttally_exhausted_by_overvote'],
+            'total_posttally_exhausted_by_skipped_rankings': stats['total_posttally_exhausted_by_skipped_rankings'],
+            'total_posttally_exhausted_by_abstention': stats['total_posttally_exhausted_by_abstention'],
+            'total_posttally_exhausted_by_duplicate_rankings': stats['total_posttally_exhausted_by_duplicate_rankings'],
+            'total_posttally_exhausted_by_rank_limit': stats['total_posttally_exhausted_by_rank_limit'],
+            'total_posttally_exhausted_by_rank_limit_fully_ranked': stats['total_posttally_exhausted_by_rank_limit_fully_ranked'],
+            'total_posttally_exhausted_by_rank_limit_partially_ranked': stats['total_posttally_exhausted_by_rank_limit_partially_ranked'],
+            'first_round_winner_vote': stats['first_round_winner_vote'],
+            'final_round_winner_vote': stats['final_round_winner_vote'],
+            'first_round_winner_percent': stats['first_round_winner_percent'],
+            'final_round_winner_percent': stats['final_round_winner_percent'],
+            'first_round_winner_place': stats['first_round_winner_place'],
         })
 
     return data
@@ -87,7 +146,9 @@ if __name__ == '__main__':
     args = get_args()
 
     file_names = [
-        'Moab_11022021_CityCouncil.csv'
+        'Moab_11022021_CityCouncil.csv',
+        # 'Vineyard_11022021_CityCouncil.csv',
+        # 'WoodlandHills_11022021_CityCouncil.csv',
     ]
 
     data = parse_cvrs(file_names)
