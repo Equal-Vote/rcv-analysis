f305f1dc3c2afab4e0e8f170182196ad5091a28e
Arend Peter Castelein <apc1993@gmail.com>
2023-10-26 09:51:43 -0700

    Add error catching

--- parser.py
+++ parser.py
@@ -75,63 +75,70 @@ def parse_cvrs(file_names):
         log(f'{i+1}/{len(file_names)} {election_name}....', end='')
         start = time.time()
 
-        # Load election into rcv_cruncher
-        base_path = Path.cwd() / 'cvr';
-        cvr_file = base_path / file;
+        try:
+            # Load election into rcv_cruncher
+            base_path = Path.cwd() / 'cvr';
+            cvr_file = base_path / file;
 
-        election = SingleWinner(
-             parser_func=rank_column_csv,
-             parser_args={'cvr_path': cvr_file},
-             exhaust_on_duplicate_candidate_marks=False,
-             exhaust_on_overvote_marks=True,
-             exhaust_on_N_repeated_skipped_marks=0
-        )
+            election = SingleWinner(
+                 parser_func=rank_column_csv,
+                 parser_args={'cvr_path': cvr_file},
+                 exhaust_on_duplicate_candidate_marks=False,
+                 exhaust_on_overvote_marks=True,
+                 exhaust_on_N_repeated_skipped_marks=0
+            )
 
-        stats = election.get_stats()[0].to_dict()
-        stats = {k:v[0] for k,v in stats.items()}
+            stats = election.get_stats()[0].to_dict()
+            stats = {k:v[0] for k,v in stats.items()}
 
-        # Compute competitive ratio
-        rounds = election.get_round_by_round_dict()
-            # TODO: I probably shouldn't assume that the elected will be first
-        winner = rounds['results'][-1]['tallyResults'][0]['elected'] 
-        bronze = rounds['results'][-2]['tallyResults'][0]['eliminated']
-        top3_tally = rounds['results'][-2]['tally']
-        competitive_ratio = int(top3_tally[bronze])/int(top3_tally[winner])
+            # Compute competitive ratio
+            rounds = election.get_round_by_round_dict()
+            if len(rounds['results']) < 2:
+                competitive_ratio = 0
+            else:
+                # TODO: I probably shouldn't assume that the elected will be first
+                winner = rounds['results'][-1]['tallyResults'][0]['elected'] 
+                bronze = rounds['results'][-2]['tallyResults'][0]['eliminated']
+                top3_tally = rounds['results'][-2]['tally']
+                competitive_ratio = int(top3_tally[bronze])/int(top3_tally[winner])
 
-        # Generate row
-        data[election_name].update({
-            'election': election_name,
-            'competitive_ratio': competitive_ratio,
+            # Generate row
+            data[election_name].update({
+                'election': election_name,
+                'competitive_ratio': competitive_ratio,
 
-            'n_candidates': stats['n_candidates'],
-            'rank_limit': stats['rank_limit'],
-            'restrictive_rank_limit': stats['restrictive_rank_limit'],
-            'first_round_overvote': stats['first_round_overvote'],
-            'ranked_single': stats['ranked_single'],
-            'ranked_3_or_more': stats['ranked_3_or_more'],
-            'ranked_multiple': stats['ranked_multiple'],
-            'total_ballots': stats['total_ballots'],
-            'total_fully_ranked': stats['total_fully_ranked'],
-            'mean_rankings_used': stats['mean_rankings_used'],
-            'winner': stats['winner'],
-            'n_rounds': stats['n_rounds'],
-            'total_pretally_exhausted': stats['total_pretally_exhausted'],
-            'total_posttally_exhausted': stats['total_posttally_exhausted'],
-            'total_posttally_exhausted_by_overvote': stats['total_posttally_exhausted_by_overvote'],
-            'total_posttally_exhausted_by_skipped_rankings': stats['total_posttally_exhausted_by_skipped_rankings'],
-            'total_posttally_exhausted_by_abstention': stats['total_posttally_exhausted_by_abstention'],
-            'total_posttally_exhausted_by_duplicate_rankings': stats['total_posttally_exhausted_by_duplicate_rankings'],
-            'total_posttally_exhausted_by_rank_limit': stats['total_posttally_exhausted_by_rank_limit'],
-            'total_posttally_exhausted_by_rank_limit_fully_ranked': stats['total_posttally_exhausted_by_rank_limit_fully_ranked'],
-            'total_posttally_exhausted_by_rank_limit_partially_ranked': stats['total_posttally_exhausted_by_rank_limit_partially_ranked'],
-            'first_round_winner_vote': stats['first_round_winner_vote'],
-            'final_round_winner_vote': stats['final_round_winner_vote'],
-            'first_round_winner_percent': stats['first_round_winner_percent'],
-            'final_round_winner_percent': stats['final_round_winner_percent'],
-            'first_round_winner_place': stats['first_round_winner_place'],
-        })
+                'n_candidates': stats['n_candidates'],
+                'rank_limit': stats['rank_limit'],
+                'restrictive_rank_limit': stats['restrictive_rank_limit'],
+                'first_round_overvote': stats['first_round_overvote'],
+                'ranked_single': stats['ranked_single'],
+                'ranked_3_or_more': stats['ranked_3_or_more'],
+                'ranked_multiple': stats['ranked_multiple'],
+                'total_ballots': stats['total_ballots'],
+                'total_fully_ranked': stats['total_fully_ranked'],
+                'mean_rankings_used': stats['mean_rankings_used'],
+                'winner': stats['winner'],
+                'n_rounds': stats['n_rounds'],
+                'total_pretally_exhausted': stats['total_pretally_exhausted'],
+                'total_posttally_exhausted': stats['total_posttally_exhausted'],
+                'total_posttally_exhausted_by_overvote': stats['total_posttally_exhausted_by_overvote'],
+                'total_posttally_exhausted_by_skipped_rankings': stats['total_posttally_exhausted_by_skipped_rankings'],
+                'total_posttally_exhausted_by_abstention': stats['total_posttally_exhausted_by_abstention'],
+                'total_posttally_exhausted_by_duplicate_rankings': stats['total_posttally_exhausted_by_duplicate_rankings'],
+                'total_posttally_exhausted_by_rank_limit': stats['total_posttally_exhausted_by_rank_limit'],
+                'total_posttally_exhausted_by_rank_limit_fully_ranked': stats['total_posttally_exhausted_by_rank_limit_fully_ranked'],
+                'total_posttally_exhausted_by_rank_limit_partially_ranked': stats['total_posttally_exhausted_by_rank_limit_partially_ranked'],
+                'first_round_winner_vote': stats['first_round_winner_vote'],
+                'final_round_winner_vote': stats['final_round_winner_vote'],
+                'first_round_winner_percent': stats['first_round_winner_percent'],
+                'final_round_winner_percent': stats['final_round_winner_percent'],
+                'first_round_winner_place': stats['first_round_winner_place'],
+            })
 
-        log(f'{round(time.time()-start)}s')
+            log(f'{round(time.time()-start)}s')
+        except Exception as e:
+            log('ERROR')
+
 
     return data
 
@@ -165,7 +172,7 @@ if __name__ == '__main__':
 
     data = parse_cvrs(file_names)
 
-    add_overrides(data)
+    #add_overrides(data)
 
     log(f'Total Time: {round(time.time()-start)}s')
 
